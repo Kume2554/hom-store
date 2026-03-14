945da9cd047c9dd30d06239ff452a2ebeca209ba
Kume2554 <applexd62@gmail.com>
2026-03-14 13:13:27 +0700

    Add files via upload

--- cart/urls.py
+++ cart/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cart_summary, name='cart_summary'),
-    path('add/', views.cart_add, name='cart_add'),
-    path('delete/', views.cart_delete, name='cart_delete'),
-    path('update/', views.cart_update, name='cart_update'), # <-- เพิ่มบรรทัดนี้
+    path('add/<int:id>/', views.cart_add, name='cart_add'),
+    path('delete/<int:id>/', views.cart_delete, name='cart_delete'),
+    path('update/<int:id>/', views.cart_update, name='cart_update'),
     path('checkout/', views.checkout, name='checkout'),
 ]
--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
