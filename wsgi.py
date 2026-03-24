2dac0e98cda43eeb78112591fe9a377ee615fc1f
Kume2554 <applexd62@gmail.com>
2026-03-25 00:26:32 +0700

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# ต้องระบุชื่อโฟลเดอร์ที่เก็บ settings.py ให้ถูก
+# ตรงนี้ต้องเป็น 'homstore.settings' (ใช้จุด . เท่านั้น ห้ามใช้ /)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homstore.settings')
 
 application = get_wsgi_application()
