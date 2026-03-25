b8969db68dda7889e9b74ac5b9d47510fe6ae25e
Kume2554 <applexd62@gmail.com>
2026-03-25 17:09:13 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -3,4 +3,10 @@ from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'templates/shop/product_list.html', {'products': products})
+    return render(request, 'shop/product_list.html', {'products': products})
+
+def login_user(request):
+    return render(request, 'shop/login.html' , {})
+
+def logout_user(request):
+    return render(request, 'shop/logout.html' , {})
