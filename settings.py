6e2d16966802b0cfae389cc1b8714f6325bf804a
Kume2554 <applexd62@gmail.com>
2026-03-25 16:12:52 +0700

    Add files via upload

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-homstore-key-2026'
-DEBUG = True 
+DEBUG = True
 
-ALLOWED_HOSTS = ['*'] # สำคัญมากเพื่อให้ Render ยอมให้เข้าเว็บ
+ALLOWED_HOSTS = ['*']  # สำคัญมากเพื่อให้ Render ยอมให้เข้าเว็บ
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -14,13 +14,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
-    'cart',
+    # Add your apps here
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # สำหรับจัดการไฟล์ CSS/รูปภาพ
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # สำหรับจัดการไฟล์ CSS/รูปภาพ
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -31,6 +30,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'homstore.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'homstore.wsgi.application'
 
 DATABASES = {
@@ -40,6 +55,20 @@ DATABASES = {
     }
 }
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
